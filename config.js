69718a0ee5a094ff3be22e74be22b8d0e7147200
phuccoder <anhdeptraippt@gmail.com>
2024-06-03 12:42:07 +0700

    Add cors

--- config.js
+++ config.js
@@ -1,52 +1,68 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './src/routes/userRoutes.js';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./src/routes/userRoutes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import cors from "cors";
 
-const app = express();
 dotenv.config();
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow only your frontend domain
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
 
 // Connect to DB
 const PORT = process.env.PORT || 8000;
 const MONGOURL = process.env.MONGO_URL;
 
-mongoose.connect(MONGOURL).then(() => {
-  console.log("Database connected successfully.");
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+mongoose
+  .connect(MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
   });
-})
-.catch((err) => console.log(err));
 
-
-
-
-//Swagger
+// Swagger
 
 const swaggerOptions = {
   swaggerDefinition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Express API with Swagger',
-      version: '1.0.0',
-      description: 'This is a simple CRUD API application made with Express and documented with Swagger',
+      title: "Express API with Swagger",
+      version: "1.0.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
     },
     servers: [
       {
-        url: 'https://testdb-fajc.onrender.com',
+        url: "https://testdb-fajc.onrender.com",
       },
     ],
   },
   // Update here to include the path to your controllers
-  apis: ['./src/routes/*.js', './src/controllers/*.js'],
+  apis: ["./src/routes/*.js", "./src/controllers/*.js"],
 };
 
-app.use('/allusers', userRoutes);
 const specs = swaggerJsdoc(swaggerOptions);
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(specs, { explorer: true })
 );
+
+app.use("/allusers", userRoutes);
